240d7f08f7dc0296993a19804ce51d7cbb5e467a
Estenio Gabriel <esteniogabrieldelima@gmail.com>
2025-11-20 19:17:52 -0300

    Refatorando a funcao de categoria de pokemon do site

--- public/app.js
+++ public/app.js
@@ -15,8 +15,8 @@ const state = {
     typeFilter: '',
 };
 
-const API = 'https://pokeapi.co/api/v2/pokemon';
-const API2 = 'https://pokeapi.co/api/v2/type';
+const API = 'https://pokeapi.co/api/v2/pokemon'; // API with all pokemon
+const API2 = 'https://pokeapi.co/api/v2/type'; // API with pokemon category
 
 const domElements = {
     loading: document.getElementById('loading'),
@@ -61,7 +61,7 @@ async function i() {
     for(var i = 0; i < 20; i++) {
         document.getElementById('loading').innerHTML += '<div class="col-md-3"><div class="skeleton"></div></div>';
     }
-    
+
     try {
         var r = await fetch(API2);
         var dt = await r.json();
@@ -75,32 +75,32 @@ async function i() {
     } catch(err) {
         console.log('erro');
     }
-    
+
     l();
 }
 
 async function l() {
     document.getElementById('loading').style.display = 'flex';
     document.getElementById('pokemonGrid').style.display = 'none';
-    
+
     try {
         var off = (c - 1) * d;
         var ur = API + '?limit=' + d + '&offset=' + off;
         var r = await fetch(ur);
         var dt = await r.json();
-        
+
         var pro = [];
         for(var i = 0; i < dt.results.length; i++) {
             pro.push(fetch(dt.results[i].url));
         }
-        
+
         var r = await Promise.all(pro);
         a = [];
         for(var i = 0; i < r.length; i++) {
             var pokemon = await r[i].json();
             a.push(pokemon);
         }
-        
+
         b = [...a];
         UNIFOR();
     } catch(error) {
@@ -109,26 +109,26 @@ async function l() {
     }
 }
 
-async function lbt() {
+async function category() {
     document.getElementById('loading').style.display = 'flex';
     document.getElementById('pokemonGrid').style.display = 'none';
 
     try {
-        var ur = API2 + '/' + f1;
-        var r = await fetch(ur);
-        var dt = await r.json();
+        const pokemonCategory = API2 + '/' + f1;
+        const response = await fetch(pokemonCategory);
+        const sizePokemonList = await response.json();
+        const listPokemon = [];
 
-        var pr = [];
-        var li = dt.pokemon.length > 100 ? 100 : dt.pokemon.length; // Limita a 100
-        for(var i = 0; i < li; i++) {
-            pr.push(fetch(dt.pokemon[i].pokemon.url));
+        const limitForPage = sizePokemonList.pokemon.length > 100 ? 100 : sizePokemonList.pokemon.length; // Limita a 100
+        for(let index = 0; index < limitForPage; index++) {
+            listPokemon.push(fetch(sizePokemonList.pokemon[index].pokemon.url));
         }
 
-        var rps = await Promise.all(pr);
+        const promise = await Promise.all(listPokemon);
         a = [];
-        for(var i = 0; i < rps.length; i++) {
-            var p = await rps[i].json();
-            a.push(p);
+        for(let index = 0; index < promise.length; index++) {
+            const pokemon = await promise[index].json();
+            a.push(pokemon);
         }
 
         b = [...a];
@@ -155,22 +155,22 @@ function UNIFOR() {
         var p = fil[i];
         var fdp = document.createElement('div');
         fdp.className = 'col-md-3';
-        
+
         var html = '<div class="c" onclick="showDetails(' + p.id + ')">';
         html = html + '<img src="' + p.sprites.front_default + '" class="i" alt="' + p.name + '">';
         html = html + '<h5 class="text-center">#' + p.id + ' ' + p.name.charAt(0).toUpperCase() + p.name.slice(1) + '</h5>';
         html = html + '<div class="text-center">';
-        
+
         for(var j = 0; j < p.types.length; j++) {
             var typeName = p.types[j].type.name;
             html = html + '<span class="badge type-' + typeName + '">' + typeName + '</span> ';
         }
-        
+
         html = html + '</div></div>';
         fdp.innerHTML = html;
         g.appendChild(fdp);
     }
-    
+
     document.getElementById('loading').style.display = 'none';
     document.getElementById('pokemonGrid').style.display = 'flex';
 
@@ -190,7 +190,7 @@ async function f() {
 
     // Se tem filtro de tipo, busca pokémons daquele tipo
     if(f1 !== '') {
-        await lbt();
+        await category();
     } else {
         UNIFOR();
     }
@@ -233,10 +233,10 @@ async function Minhe_nha(id) {
     try {
         var xpto = await fetch(API + '/' + id);
         var p = await xpto.json();
-        
+
         var zyz = await fetch(p.species.url);
         var m = await zyz.json();
-        
+
         var desc = '';
         for(var i = 0; i < m.flavor_text_entries.length; i++) {
             if(m.flavor_text_entries[i].language.name === 'en') {
@@ -244,36 +244,36 @@ async function Minhe_nha(id) {
                 break;
             }
         }
-        
+
         document.getElementById('modalTitle').textContent = '#' + p.id + ' ' + p.name.charAt(0).toUpperCase() + p.name.slice(1);
-        
+
         var ph = '<div class="row"><div class="col-md-6">';
         ph += '<div class="sprite-container">';
         ph += '<div><img src="' + p.sprites.front_default + '" alt="front"><p class="text-center">Normal</p></div>';
         ph += '<div><img src="' + p.sprites.front_shiny + '" alt="shiny"><p class="text-center">Shiny</p></div>';
         ph += '</div>';
-        
+
         ph += '<p><strong>Tipo:</strong> ';
         for(var i = 0; i < p.types.length; i++) {
             ph += '<span class="badge type-' + p.types[i].type.name + '">' + p.types[i].type.name + '</span> ';
         }
         ph += '</p>';
-        
+
         ph += '<p><strong>Altura:</strong> ' + (p.height / 10) + ' m</p>';
         ph += '<p><strong>Peso:</strong> ' + (p.weight / 10) + ' kg</p>';
-        
+
         ph += '<p><strong>Habilidades:</strong> ';
         for(var i = 0; i < p.abilities.length; i++) {
             ph += p.abilities[i].ability.name;
             if(i < p.abilities.length - 1) ph += ', ';
         }
         ph += '</p>';
-        
+
         ph += '</div><div class="col-md-6">';
-        
+
         ph += '<p><strong>Descrição:</strong></p>';
         ph += '<p>' + desc.replace(/\f/g, ' ') + '</p>';
-        
+
         ph += '<h6>Estatísticas:</h6>';
         for(var i = 0; i < p.stats.length; i++) {
             var stat = p.stats[i];
@@ -281,14 +281,14 @@ async function Minhe_nha(id) {
             ph += '<div><small>' + stat.stat.name + ': ' + stat.base_stat + '</small>';
             ph += '<div class="stat-bar"><div class="stat-fill" style="width: ' + percentage + '%"></div></div></div>';
         }
-        
+
         ph += '</div></div>';
-        
+
         document.getElementById('modalBody').innerHTML = ph;
-        
+
         var mod = new bootstrap.Modal(document.getElementById('m'));
         mod.show();
-        
+
     } catch(error) {
         console.log('erro');
         alert('Erro ao carregar detalhes!');
